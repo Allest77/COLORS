5bd0fd1c8567f6562d5d39d4de27c2de3ea16268
Allest77 <andr0idph0ne6969@gmail.com>
2021-10-21 13:18:41 -0400

    Fixed Broken Project

---
+++ Assets/Scripts/Power Up Scripts/PowerUpAbilities.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpAbilities : MonoBehaviour {
+
+}
+    //public class PowerUpAbilities : PlayerController {
+        //[SerializeField] private PlayerController playerController;
+        //public bool doubleJump = false;
+        //Rigidbody rb;
+
+        //List of Power Ups
+    //public void DoubleJump() {
+        //Rigidbody rb.velocity = new Vector3(Rigidbody.velocity.x, Rigidbody.velocity.y, 0);
+        //Rigidbody rb.AddForce(new Vector3(0f, jumpForce, 0f));
+
+        //if (!isGrounded)
+            //doubleJump = true;
+        //}
+
+        //Insert other power ups here
+    //}
