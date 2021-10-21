a8ceec33163c6925de7f5567e871131481b8f8b7
Allest77 <andr0idph0ne6969@gmail.com>
2021-10-20 20:00:41 -0400

    Jumping WORKs
    Added stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,32 +5,55 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
     public CharacterController controller;
-    public float speed = 8, jumpForce = 10, gravity = -20;
+    public float speed = 8f, jumpHeight = 5f, jumpForce = 20, gravity = -20.0f, groundDistance = 0.4f, gravityMod = 4;
     public Transform groundCheck;
     public LayerMask groundLayer;
     public int points = 0;
     public Text coins;
-    private Vector3 direction;
+    public bool isGrounded = true, hasPowerup = false;
+    public GameObject powerupIndicator;
+    private Vector3 direction, gravityNormal, gravityFast;
 
-    void Start()
-    {
-        
-    }
+    Rigidbody rb;
+
+    void Start() { 
+        rb = GetComponent<Rigidbody>();
+        gravityNormal = Physics.gravity;
+        gravityFast = gravityNormal * gravityMod; }
 
     void Update() {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        if (!isGrounded) {
+            direction.y += gravity * Time.deltaTime; }
+        
+        //Move horizontally.
         float hInput = Input.GetAxis("Horizontal");
         direction.x = hInput * speed;
 
-    bool isGrounded = Physics.CheckSphere(groundCheck.position, 0.15f, groundLayer);
-        direction.y += gravity * Time.deltaTime;
-        if (isGrounded) {
-            if (Input.GetButtonDown("Jump")) {
-                direction.y = jumpForce;
-            }
-        }
+        //Jump.
+        if(Input.GetButtonDown("Jump") && isGrounded) {
+            direction.y = (jumpHeight * jumpForce); }
+
+        //Gravity Modifier.
+        if(rb.velocity.y < 0) {
+            Physics.gravity = gravityMod * gravityFast; }
+
+        //Move while airbone.
+        float forwardInput = Input.GetAxis("Vertical");
+        rb.AddForce(Vector3.up * forwardInput * speed);
 
         controller.Move(direction * Time.deltaTime);
 
-        coins.text = "x : " + points;
+        //coins.text = "x : " + points;
     }
+
+    //Obtain Power Up
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Powerup")) {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true); } }
+
+    //List of Power Ups
 }
