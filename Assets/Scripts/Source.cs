505972a0ef1bda19b5e4de7392cc7822fced90e2
Allest77 <andr0idph0ne6969@gmail.com>
2021-10-20 23:31:21 -0400

    Game
    A lot

--- Assets/Scripts/Source.cs
+++ Assets/Scripts/Source.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Source : MonoBehaviour {
-    public Image collect;
-    // Update is called once per frame
+public class Source : MonoBehaviour { 
+    public GameObject pickupEffect;
+    public float jumpMultiplier = 1.4f;
+
     void Update() {
         transform.Rotate(0, 0, 90 * Time.deltaTime); }
 
-    private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Player")) {
-            collect.gameObject.SetActive(true);
-            Destroy(gameObject); //insert respawn item here after being destroyed for x time
-        }
-    }
+    //private void OnTriggerEnter(Collider other) {
+        //if (other.gameObject.CompareTag("Player")) {
+            //PickUp(other); } }
+
+    //void PickUp(Collider player)
+    //{
+        //Pink power up
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+        //PlayerController jump = player.GetComponent<jumpHeight>();
+        //jump.jumpHeight *= jumpMultiplier;
+        //Destroy(gameObject);
+    //}
 }
